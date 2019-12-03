3ce5226056f1dbcc24053beb80fba3dc75fc039a
Jen and Christine <christine.braun13@gmail.com>
2019-12-03 10:02:14 -0800

    Delete unnecessary files and create game class

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './Menu';
+import Game from './Game';
+
+function App(){
+  return(
+    <div>
+      <Menu />
+      <Game />
+    </div>
+  )
+}
+export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,13 +2,46 @@ import React from 'react';
 import ScoreBoard from './ScoreBoard';
 import PlayArea from './PlayArea';
 
-function Game(props) {
-  return (
-    <div>
-      <ScoreBoard />
-      <PlayArea />
-    </div>
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      gameBoard: emptyGameBoard(8),
+      score: 0
+    }
+  }
+
+  emptyGameBoard(size){
+    let gameBoard = [];
+    for(let i =0; i < size; i++)
+    {
+      row=[];
+      for(let j = 0; j<size; j++)
+      {
+        row.push('');
+      }
+      gameBoard.push(row);
+    }
+    return gameBoard;
+  }
+
+  handleStartClick(){
+
+  }
+  handleRulesClick(){
+    
+  }
+
+  render(){
+    return (
+      <div>
+        <ScoreBoard score={this.state.score}/>
+        <PlayArea gameBoard={this.state.gameBoard}/>
+      </div>
     )
+  }
+
+  
   }
   
 export default Game;
