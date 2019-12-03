ad897f873d0b2d5373bf69924ee2bb4fc4658ef6
Student <example@example.com>
2019-12-03 09:13:30 -0800

    Add more config for redux, babel and eslint

---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+
+const render = (Component) => {
+  ReactDOM.render(
+    <HashRouter>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </HashRouter>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
